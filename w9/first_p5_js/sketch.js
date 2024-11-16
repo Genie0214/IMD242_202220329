713572837282616f05b87f1fdaefdf6208450ab6
ujhj01- <163325481+Genie0214@users.noreply.github.com>
2024-11-17 05:06:42 +0900

    ./mid_project1/Bubble/Bubble.pde

--- w9/first_p5_js/sketch.js
+++ w9/first_p5_js/sketch.js
@@ -1,8 +1,29 @@
+let tileNumX;
+let tileNumY;
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 100);
+  noStroke();
+  fill('red');
+  // for (let colume = 0; colume < width; colume += 40) {
+  //   for (let row = 0; row < width; row += 40) {
+  //     let x = 20 + colume;
+  //     let y = 20 + row;
+  //     let diameter = 30;
+  //     circle(x, y, diameter);
+  //   }
+  // }
+  for (let row = 0; row < tileNumY; row++) {
+    for (let colume = 0; row < tileNumX; colume++) {
+      let tileW = width / tileNumX;
+      let tileH = height / tileNumY;
+      let x = tileW * 0.5 + colume * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
 }
